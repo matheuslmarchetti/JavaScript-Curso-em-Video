309c4c72f7e4f1be16df43905866eea5af636dc8
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-02-27 14:40:11 -0400

    Update ex008.js

---
+++ classroom11/ex008.js
@@ -0,0 +1,6 @@
+var speed = 78
+console.log(`The speed your car is ${speed}Km/h.`);
+if (speed > 60) {
+    console.log(`Vou exceeded the allowed speed. FINED!`);
+}
+console.log(`Drive ever using seat belt!`);
