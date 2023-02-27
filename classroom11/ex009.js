b229780d6ef0b559bc46b1a7e5085b4054e6631f
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-02-27 15:44:00 -0400

    Update

--- classroom11/ex009.js
+++ classroom11/ex009.js
@@ -1,7 +1,7 @@
 var country = "Brazil"
 console.log(`Living in the ${country}.`)
 if (country != "Brazil") {
-    console.log(" You are Foreigner.");
+    console.log("You are Foreigner.");
 } else {
     console.log("You are Brazilian.")
 }
