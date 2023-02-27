b1f02d165614a5b7c96cd6074c7ae1ece6b26b45
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-02-27 16:06:22 -0400

    Update

---
+++ classroom04/ex001.js
@@ -0,0 +1,3 @@
+window.alert('This is my first message');
+window.alert('Are you enjoying JS?');
+window.prompt('What is your name?');
---
+++ classroom06/ex002.js
@@ -0,0 +1,4 @@
+var name = window.prompt('What is your name?');
+window.alert(`Welcome ${name}`);
+var promptname = document.querySelector(".promptname");
+promptname.textContent = name;
