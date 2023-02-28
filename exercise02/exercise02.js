37e5afb76b5528fea5499b973ccba2952d3779ce
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-02-28 15:36:43 -0400

    Update

--- exercise02/exercise02.js
+++ exercise02/exercise02.js
@@ -1,7 +1,21 @@
-function toLoad() {
+function load() {
     var msg = document.querySelector("div#msg")
     var photos = document.querySelector("div#dawn")
     var date = new Date()
-    var hour = date.getHours()
+    //var hour = date.getHours()
+    var hour = 17
     msg.innerHTML = `Now are ${hour} hours.`
+    if (hour >= 6 && hour < 12) {
+        //Morning!
+        document.getElementById("imgdefault").src = "https://source.unsplash.com/random/?sunrise";
+    } else if (hour >= 12 && hour < 16) {
+        //Afternoon!
+        document.getElementById("imgdefault").src = "https://source.unsplash.com/random/?sun";
+    } else if (hour >= 16 && hour <= 18) {
+        //Dusk!
+        document.getElementById("imgdefault").src = "https://source.unsplash.com/random/?dusk";
+    } else {
+        //Night!
+        document.getElementById("imgdefault").src = "https://source.unsplash.com/random/?night"
+    }
 }
