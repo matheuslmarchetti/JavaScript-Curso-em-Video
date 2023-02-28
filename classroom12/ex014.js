b1209db91960d4e97ac0b8958dd3b8272d5d97bd
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-02-28 10:23:10 -0400

    Update ex014.js

---
+++ classroom12/ex014.js
@@ -0,0 +1,27 @@
+var now = new Date()
+var dayOfTheWeek = now.getDay()
+switch (dayOfTheWeek) {
+    case 0:
+        console.log("Sunday");
+        break;
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+    default:
+        console.log("Error! Invalid Day");
+        break;
+}
