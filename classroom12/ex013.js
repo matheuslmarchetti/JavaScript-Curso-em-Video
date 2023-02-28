3f08b5ec181684785cdba07483474900c48c8115
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-02-28 08:22:16 -0400

    Update ex013.js

--- classroom12/ex013.js
+++ classroom12/ex013.js
@@ -1,1 +1,10 @@
-var hour = 8
+var now = new Date()
+var hour = now.getHours()
+console.log(`Now are exactly ${hour} hours.`)
+if (hour < 12) {
+    console.log("Good Morning!");
+} else if (hour <= 18) {
+    console.log("Good Afternoon!");
+} else {
+    console.log("Good Night!")
+}
