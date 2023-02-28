4b27213d4992b54b806fa75814dce04244a151db
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-02-28 14:22:17 -0400

    Update exercise02.js

--- exercise02/exercise02.js
+++ exercise02/exercise02.js
@@ -1,4 +1,7 @@
-function toload() {
+function toLoad() {
     var msg = document.querySelector("div#msg")
     var photos = document.querySelector("div#dawn")
+    var date = new Date()
+    var hour = date.getHours()
+    msg.innerHTML = `Now are ${hour} hours.`
 }
