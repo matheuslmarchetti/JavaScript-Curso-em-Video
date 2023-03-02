ab35354d433ccffe4c44a43585ceee62af289686
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-03-02 09:47:57 -0400

    Updates

---
+++ exercise03/exercise03.js
@@ -0,0 +1,6 @@
+function toChecker() {
+    var date = new Date()
+    var year = date.getFullYear()
+    var formYear = document.querySelector("p#txtyear")
+    var result = document.querySelector("div#result")
+}
