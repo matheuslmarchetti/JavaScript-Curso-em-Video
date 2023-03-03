06e2248f7430c1cb1d923f6602ffcdc742349b6f
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-03-03 13:49:40 -0400

    Update exercise04.js

--- exercise04/exercise04.js
+++ exercise04/exercise04.js
@@ -2,10 +2,30 @@ function letCount() {
     let start = document.querySelector("input#txtstart")
     let end = document.querySelector("input#txtend")
     let steps = document.querySelector("input#txtsteps")
-    let result = querySelector("div#result")
+    let result = document.querySelector("div#result")
     if (start.value.length == 0 || end.value.length == 0 || steps.value.length == 0) {
-        alert("ERRO!")
+        result.innerHTML = "Impossible to count!"
+        alert("ERRO!");
     } else {
         result.innerHTML = "Counting:"
+        let startvalue = Number(start.value)
+        let endvalue = Number(end.value)
+        let stepsvalue = Number(steps.value)
+        if (stepsvalue <= 0) {
+            alert("Invalid step! Considering Steps = 1.");
+            startvalue = 1
+        }
+        if (startvalue < endvalue) {
+            //Count Up
+            for (let counter = startvalue; counter <= endvalue; counter += stepsvalue) {
+                result.innerHTML += ` ${counter} \u{1F449}`;
+            }
+        } else {
+            //Countdown
+            for (let counter = startvalue; counter >= endvalue; counter -= stepsvalue) {
+                result.innerHTML += ` ${counter} \u{1F449}`;
+            }
+        }
+        result.innerHTML += `\u{1F3C1}`
     }
 }
