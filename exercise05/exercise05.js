15c855ee679cb84c3cbcc7093deb32e425650f1b
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-03-03 17:23:48 -0400

    Update

--- exercise05/exercise05.js
+++ exercise05/exercise05.js
@@ -1,1 +1,26 @@
-/* alert("Hello!") */
+function toGenerate() {
+    let number = document.querySelector("input#txtnumber");
+    let message = document.querySelector("p#msg");
+    if (number.value.length == "") {
+        alert("ERROR! Click on 'OK' to receive the explanation.");
+        message.innerHTML = "You need to choose a number bigger than 0. Try again!";
+        multiplicationresult.innerHTML = "";
+    } else if (Number(number.value) == 0 || Number(number.value) < 0) {
+        alert("ERROR! Click on 'OK' to receive the explanation.");
+        message.innerHTML = `You chose ${Number(number.value)}. You need to choose a number bigger than 0. Try again!`;
+        multiplicationresult.innerHTML = "";
+    } else {
+        message.innerHTML = `This is the result of the ${Number(number.value)} Multiplication Table. Check it out below!`;
+        let numberValue = Number(number.value);
+        let multiplicationresult = document.querySelector
+        ("select#multiplicationresult");
+        multiplicationresult.innerHTML = "";
+        for (let counter = 0; counter <= 10; counter++) {
+            let select = document.createElement("select")
+            select.setAttribute("id", "multiplicationresult")
+            let item = document.createElement("option");
+            item.text = `${numberValue} x ${counter} = ${numberValue*counter}`;
+            multiplicationresult.appendChild(item);
+        }
+    }
+}
