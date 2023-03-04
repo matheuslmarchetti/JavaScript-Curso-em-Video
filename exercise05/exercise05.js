36e352bbc30e15c33f162794ac78728a42102efa
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-03-04 00:14:36 -0400

    Update

--- exercise05/exercise05.js
+++ exercise05/exercise05.js
@@ -4,23 +4,41 @@ function toGenerate() {
     if (number.value.length == "") {
         alert("ERROR! Click on 'OK' to receive the explanation.");
         message.innerHTML = "You need to choose a number bigger than 0. Try again!";
-        multiplicationresult.innerHTML = "";
+        //Block to clear the <select/>
+        let newDiv = document.createElement("div");
+        newDiv.setAttribute("id", "returnresult");
+        let newDivContent = document.createTextNode("The result will go appear here.");
+        newDiv.appendChild(newDivContent);
+        let theSelect = document.querySelector("select#multiplicationresult");
+        let parentTheSelect = theSelect.parentNode;
+        parentTheSelect.replaceChild(newDiv, theSelect);
+        //Block to clear the <select/>
     } else if (Number(number.value) == 0 || Number(number.value) < 0) {
         alert("ERROR! Click on 'OK' to receive the explanation.");
         message.innerHTML = `You chose ${Number(number.value)}. You need to choose a number bigger than 0. Try again!`;
-        multiplicationresult.innerHTML = "";
+        //Block to clear the <select/>
+        let newDiv = document.createElement("div");
+        newDiv.setAttribute("id", "returnresult");
+        let newDivContent = document.createTextNode("The result will go appear here.");
+        newDiv.appendChild(newDivContent);
+        let theSelect = document.querySelector("select#multiplicationresult");
+        let parentTheSelect = theSelect.parentNode;
+        parentTheSelect.replaceChild(newDiv, theSelect);
+        //Block to clear the <select/>
     } else {
         message.innerHTML = `This is the result of the ${Number(number.value)} Multiplication Table. Check it out below!`;
         let numberValue = Number(number.value);
-        let multiplicationresult = document.querySelector
-        ("select#multiplicationresult");
-        multiplicationresult.innerHTML = "";
+        let createSelect = document.createElement("select");
+        createSelect.setAttribute("id", "multiplicationresult");
+        createSelect.setAttribute("size", 20);
+        let divResultMsg = document.querySelector("div#resultmsg");
+        let parentDivResultMsg = divResultMsg.parentNode;
+        parentDivResultMsg.replaceChild(createSelect, divResultMsg);
+        //multiplicationresult.innerHTML = "";
         for (let counter = 0; counter <= 10; counter++) {
-            let select = document.createElement("select")
-            select.setAttribute("id", "multiplicationresult")
             let item = document.createElement("option");
             item.text = `${numberValue} x ${counter} = ${numberValue*counter}`;
-            multiplicationresult.appendChild(item);
+            createSelect.appendChild(item);
         }
     }
 }
