4fed580183c1011a2a425803dcfe60e15c78455a
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-03-04 04:16:58 -0400

    Update

--- exercise05/exercise05.js
+++ exercise05/exercise05.js
@@ -1,32 +1,28 @@
 function toGenerate() {
     let number = document.querySelector("input#txtnumber");
     let message = document.querySelector("p#msg");
+    let theResult = document.querySelector("select#result");
     if (number.value.length == "") {
         alert("ERROR! Click on 'OK' to receive the explanation.");
-        message.innerHTML = "You need to choose a number bigger than 0. Try again!";
-        //Block to clear the <select/>
-        /* let newDiv = document.createElement("div");
-        newDiv.setAttribute("id", "returnresult");
-        let newDivContent = document.createTextNode("The result will go appear here.");
-        newDiv.appendChild(newDivContent);
-        let theSelect = document.querySelector("select#multiplicationresult");
-        let parentTheSelect = theSelect.parentNode;
-        parentTheSelect.replaceChild(newDiv, theSelect); */
-        //Block to clear the <select/>
+        message.innerHTML = "You need to choose a number. Try again!";
+        theResult.innerHTML = ""
+        let divNew = document.createElement("div");
+        let divNewContent = document.createTextNode("The result will appear here.");
+        divNew.appendChild(divNewContent);
+        let theSelect = document.getElementById("result");
+        parentTheSelect = theSelect.parentNode;
+        parentTheSelect.replaceChild(divNew, theSelect);
     } else {
         message.innerHTML = `This is the result of the ${Number(number.value)} Multiplication Table. Check it out below!`;
         let numberValue = Number(number.value);
-        let createSelect = document.createElement("select");
-        createSelect.setAttribute("id", "multiplicationresult");
-        createSelect.setAttribute("size", 20);
-        let divResultMsg = document.querySelector("div#resultmsg");
-        let parentDivResultMsg = divResultMsg.parentNode;
-        parentDivResultMsg.replaceChild(createSelect, divResultMsg);
-        //multiplicationresult.innerHTML = "";
-        for (let counter = 0; counter <= 10; counter++) {
+        let counter = 0;
+        theResult.innerHTML = ""
+        while (counter <= 10) {
             let item = document.createElement("option");
             item.text = `${numberValue} x ${counter} = ${numberValue*counter}`;
-            createSelect.appendChild(item);
+            item.value = `theResult${counter}`
+            theResult.appendChild(item);
+            counter++
         }
     }
 }
