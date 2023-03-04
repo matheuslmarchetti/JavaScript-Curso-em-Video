f44bba99e57a1301da68ce93d1850a2c4d888cf9
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-03-04 13:41:54 -0400

    Update

--- exercise05/exercise05.js
+++ exercise05/exercise05.js
@@ -1,6 +1,34 @@
 function toGenerate() {
     let number = document.querySelector("input#txtnumber");
     let message = document.querySelector("p#msg");
+    let theResult = document.querySelector("ul#result");
+    if (number.value.length == "") {
+        alert("ERROR! Click on 'OK' to receive the explanation.");
+        message.innerHTML = "You need to choose a number. Try again!";
+        theResult.innerHTML = ""
+        let item = document.createElement("li");
+        let itemtext = document.createTextNode("The result will appear here.");
+        item.appendChild(itemtext);
+        let position = document.getElementsByTagName("ul")[0];
+        position.appendChild(item);
+    } else {
+        message.innerHTML = `This is the result of the ${Number(number.value)} Multiplication Table. Check it out below!`;
+        let numberValue = Number(number.value);
+        let counter = 0;
+        theResult.innerHTML = ""
+        while (counter <= 10) {
+            let item = document.createElement("li");
+            let itemtext = document.createTextNode(`${numberValue} x ${counter} = ${numberValue*counter}`);
+            item.appendChild(itemtext);
+            let position = document.getElementsByTagName("ul")[0];
+            position.appendChild(item);
+            counter++
+        }
+    }
+} 
+/* function toGenerate() {
+    let number = document.querySelector("input#txtnumber");
+    let message = document.querySelector("p#msg");
     let theResult = document.querySelector("select#result");
     if (number.value.length == "") {
         alert("ERROR! Click on 'OK' to receive the explanation.");
@@ -22,4 +50,4 @@ function toGenerate() {
             counter++
         }
     }
-}
+} */
