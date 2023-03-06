01f472d33e3563ad76641c9613a5423afbd6d4a5
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-03-06 16:00:50 -0400

    Update function.js

---
+++ classroom16/function.js
@@ -0,0 +1,11 @@
+function evenAndOdd(number) {
+    if (number%2 == 0) {
+        return "Even!";
+    } else {
+        return "Odd!";
+    }
+}
+
+console.log(evenAndOdd(7));
+/* let result = evenAndOdd(6);
+console.log(result); */
