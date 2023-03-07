b11b0cebb1b47d92a8ecff6fc2a9264bbd418860
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-03-07 13:09:41 -0400

    Update exercise06.js

--- exercise06/exercise06.js
+++ exercise06/exercise06.js
@@ -3,30 +3,35 @@ function toAdd() {
     let txtNumber = document.querySelector("#toaddnumber");
     let number = Number(txtNumber.value);
     let toCleanItem = document.querySelector("#itemlist");
+    let itemDefault = document.querySelector("#defaultitem");
     if (number == "") {
         alert("You need add a number!");
     } else if (number < 1 || number > 100) {
         alert("You need add a number between 1 and 100.");
     } else {
+        itemDefault?.remove();
         toCleanItem?.remove();
-        let item = document.createElement("li");
-        item.setAttribute("id", "newli")
+        let itemNew = document.createElement("li");
+        itemNew.setAttribute( "class", "newli");
         let itemText = document.createTextNode(`You added the number: ${number}.`);
-        item.appendChild(itemText);
+        itemNew.appendChild(itemText);
         let position = document.getElementsByTagName("ul")[0];
-        position.appendChild(item);
+        position.appendChild(itemNew);
     }
 }
 // Button Clean
 function toClean() {
-    let newli = document.querySelector("#newli");
-    if (!newli) {
-        let item = document.createElement("li");
-        item.setAttribute("id", "defaultitem")
+    let newli = document.querySelector(".newli");
+    let list = document.querySelector("#list").children;
+    let itemListFirst = list[0];
+    if (itemListFirst) {
+        itemListFirst?.remove();
+        let itemDefault = document.createElement("li");
+        itemDefault.setAttribute("id", "defaultitem")
         let itemText = document.createTextNode(`The numbers added appear here.`);
-        item.appendChild(itemText);
+        itemDefault.appendChild(itemText);
         let position = document.getElementsByTagName("ul")[0];
-        position.appendChild(item);
+        position.appendChild(itemDefault);
     } else {
         newli?.remove();
     }
