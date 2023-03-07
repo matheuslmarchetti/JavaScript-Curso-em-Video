baada61ed440407941a37e02e0f7f9828995c994
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-03-07 19:42:00 -0400

    Updates

--- exercise06/exercise06.js
+++ exercise06/exercise06.js
@@ -19,14 +19,14 @@ function inList(n, l) {
 function toAdd() {
     let number = Number(txtNumber.value);
     let toCleanItem = document.querySelector("#itemlist");
-    let results = document.querySelector("#results"); // Limpar DIV Result
+    let resultItem = document.querySelector(".itemdivresult"); // Limpar DIV Result
     if (txtNumber.value.length == "") {
         alert("You need add a number!");
     } else if (number < 1 || number > 100) {
         alert("You need add a number between 1 and 100.");
     }  else if (isNumber(number) && !inList(number, theValues)) {
         toCleanItem?.remove();
-        results?.remove(); // Limpar DIV Result
+        resultItem?.remove(); // Limpar DIV Result
         theValues.push(number);
         let itemNew = document.createElement("li");
         itemNew.setAttribute( "class", "newli");
@@ -48,10 +48,10 @@ function toClean() {
     let number = Number(txtNumber.value);
     let newli = document.querySelector(".newli");
     let firstitem = document.querySelector(".firstitem");
-    let results = document.querySelector("#results"); // Limpar DIV Result
+    let resultItem = document.querySelector(".itemdivresult"); // Limpar DIV Result
     if (newli) {
         newli?.remove() 
-        results?.remove(); // Limpar DIV Result
+        resultItem?.remove(); // Limpar DIV Result
         theValues.pop(number);
     } else if (firstitem) {
         firstitem?.remove();
@@ -62,30 +62,18 @@ function toClean() {
         itemList.appendChild(itemListText);
         let position = document.getElementsByTagName("ul")[0];
         position.appendChild(itemList);
-        results?.remove();
+        resultItem?.remove();
     }
     txtNumber.focus();
 }
-/* // Create Elements Array
-let addNumbers = document.querySelector("#toaddnumber");
-let arrayNumbers = [];
-function createArray() {
-    arrayNumbers.push(addNumbers.value);
-    console.log(arrayNumbers);
-}
-// Delete Elements Array
-function deleteArray() {
-    arrayNumbers.pop();
-    console.log(arrayNumbers);
-} */
 // Button Finalize
+let result = document.querySelector("#result");
 let buttonFinalize = document.querySelector("#buttonfinalize");
 buttonFinalize.onclick = function toFinalize() {
-    //return arrayNumbers;
-    let itemResult = document.createElement("li");
-    itemResult.setAttribute( "class", "result");
-    let itemResultText = document.createTextNode(`${createArray()}.`);
-    itemResult.appendChild(itemResultText);
-    let position = document.getElementsByTagName("ul")[1];
-    position.appendChild(itemResult);
+    if (theValues.length == 0) {
+        alert("You need to add a number, before to finalize.");
+    } else {
+        let totalElements = theValues.length;
+        result.innerHTML += `<p class="itemdivresult">We have a total of the ${totalElements} numbers registered.`
+    }   
 }
