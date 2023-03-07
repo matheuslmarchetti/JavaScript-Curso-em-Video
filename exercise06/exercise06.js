98a8faa92d761705a18bb82d17909d3b1f1cb2e6
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-03-07 16:48:27 -0400

    Update

--- exercise06/exercise06.js
+++ exercise06/exercise06.js
@@ -3,12 +3,14 @@ function toAdd() {
     let txtNumber = document.querySelector("#toaddnumber");
     let number = Number(txtNumber.value);
     let toCleanItem = document.querySelector("#itemlist");
+    let results = document.querySelector("#results"); // Limpar DIV Result
     if (number == "") {
         alert("You need add a number!");
     } else if (number < 1 || number > 100) {
         alert("You need add a number between 1 and 100.");
-    } else {
+    }  else {
         toCleanItem?.remove();
+        results?.remove(); // Limpar DIV Result
         let itemNew = document.createElement("li");
         itemNew.setAttribute( "class", "newli");
         let itemText = document.createTextNode(`You added the number: ${number}.`);
@@ -24,16 +26,19 @@ function toAdd() {
 function toClean() {
     let newli = document.querySelector(".newli");
     let firstitem = document.querySelector(".firstitem");
+    let results = document.querySelector("#results"); // Limpar DIV Result
     if (newli) {
-        newli?.remove()
+        newli?.remove() 
+        results?.remove(); // Limpar DIV Result
     } else if (firstitem) {
-        firstitem?.remove()
+        firstitem?.remove();
         let itemList = document.createElement("li");
         itemList.setAttribute( "id", "itemlist");
         let itemListText = document.createTextNode(`The numbers added appear here.`);
         itemList.appendChild(itemListText);
         let position = document.getElementsByTagName("ul")[0];
         position.appendChild(itemList);
+        results?.remove();
     }
 }
 // Create Elements Array
@@ -47,4 +52,15 @@ function createArray() {
 function deleteArray() {
     arrayNumbers.pop();
     console.log(arrayNumbers);
+}
+// Button Finalize
+let buttonFinalize = document.querySelector("#buttonfinalize");
+buttonFinalize.onclick = function toFinalize() {
+    //return arrayNumbers;
+    let itemResult = document.createElement("li");
+    itemResult.setAttribute( "class", "result");
+    let itemResultText = document.createTextNode(`${createArray()}.`);
+    itemResult.appendChild(itemResultText);
+    let position = document.getElementsByTagName("ul")[1];
+    position.appendChild(itemResult);
 }
