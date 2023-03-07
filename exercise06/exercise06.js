bc77029fd106fab658f0ef8b37da0e2a7fcb5bea
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-03-07 10:15:14 -0400

    Update exercise06.js

--- exercise06/exercise06.js
+++ exercise06/exercise06.js
@@ -1,14 +1,15 @@
-//To Add
+// Button To Add
 function toAdd() {
-    let txtNumber = document.querySelector("input#toaddnumber");
+    let txtNumber = document.querySelector("#toaddnumber");
     let number = Number(txtNumber.value);
-    let toCleanitem = document.querySelector("li#itemlist");
+    let list = document.querySelector("#list");
+    let toCleanitem = document.querySelector("#itemlist");
     if (number == "") {
         alert("You need add a number!");
     } else if (number < 1 || number > 100) {
         alert("You need add a number between 1 and 100.");
     } else {
-        toCleanitem.innerHTML = "";
+        toCleanitem?.remove();
         let item = document.createElement("li");
         let itemtext = document.createTextNode(`You added the number: ${number}.`);
         item.appendChild(itemtext);
