d275af48c01d051542a4b552a4bcae7a93276171
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-03-07 11:02:38 -0400

    Update exercise06.js

--- exercise06/exercise06.js
+++ exercise06/exercise06.js
@@ -1,19 +1,33 @@
-// Button To Add
+// Button Add
 function toAdd() {
     let txtNumber = document.querySelector("#toaddnumber");
     let number = Number(txtNumber.value);
-    let list = document.querySelector("#list");
-    let toCleanitem = document.querySelector("#itemlist");
+    let toCleanItem = document.querySelector("#itemlist");
     if (number == "") {
         alert("You need add a number!");
     } else if (number < 1 || number > 100) {
         alert("You need add a number between 1 and 100.");
     } else {
-        toCleanitem?.remove();
+        toCleanItem?.remove();
         let item = document.createElement("li");
-        let itemtext = document.createTextNode(`You added the number: ${number}.`);
-        item.appendChild(itemtext);
+        item.setAttribute("id", "newli")
+        let itemText = document.createTextNode(`You added the number: ${number}.`);
+        item.appendChild(itemText);
         let position = document.getElementsByTagName("ul")[0];
         position.appendChild(item);
+    }
+}
+// Button Clean
+function toClean() {
+    let newli = document.querySelector("#newli");
+    if (!newli) {
+        let item = document.createElement("li");
+        item.setAttribute("id", "defaultitem")
+        let itemText = document.createTextNode(`The numbers added appear here.`);
+        item.appendChild(itemText);
+        let position = document.getElementsByTagName("ul")[0];
+        position.appendChild(item);
+    } else {
+        newli?.remove();
     }
 }
