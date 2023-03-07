fa421455babf41aac96fee3d147f5bdbc82d7eea
Matheus Lunguinho Marchetti <105798967+matheuslmarchetti@users.noreply.github.com>
2023-03-07 19:11:25 -0400

    Update

--- exercise06/exercise06.js
+++ exercise06/exercise06.js
@@ -1,16 +1,33 @@
+// Settings Global
+let theValues = [];
+let txtNumber = document.querySelector("#toaddnumber");
+function isNumber(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function inList(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true;
+    } else {
+        return false;
+    }
+}
 // Button Add
 function toAdd() {
-    let txtNumber = document.querySelector("#toaddnumber");
     let number = Number(txtNumber.value);
     let toCleanItem = document.querySelector("#itemlist");
     let results = document.querySelector("#results"); // Limpar DIV Result
-    if (number == "") {
+    if (txtNumber.value.length == "") {
         alert("You need add a number!");
     } else if (number < 1 || number > 100) {
         alert("You need add a number between 1 and 100.");
-    }  else {
+    }  else if (isNumber(number) && !inList(number, theValues)) {
         toCleanItem?.remove();
         results?.remove(); // Limpar DIV Result
+        theValues.push(number);
         let itemNew = document.createElement("li");
         itemNew.setAttribute( "class", "newli");
         let itemText = document.createTextNode(`You added the number: ${number}.`);
@@ -20,18 +37,25 @@ function toAdd() {
         let list = document.querySelector("ul#list");
         let itemFirstLi = list.getElementsByTagName("li")[0];
         itemFirstLi.setAttribute("class" , "firstitem");
+    } else {
+        alert("This number already gone added! You need to insert other number.");
     }
+    txtNumber.value = "";
+    txtNumber.focus();
 }
 // Button Clean
 function toClean() {
+    let number = Number(txtNumber.value);
     let newli = document.querySelector(".newli");
     let firstitem = document.querySelector(".firstitem");
     let results = document.querySelector("#results"); // Limpar DIV Result
     if (newli) {
         newli?.remove() 
         results?.remove(); // Limpar DIV Result
+        theValues.pop(number);
     } else if (firstitem) {
         firstitem?.remove();
+        theValues.pop(number);
         let itemList = document.createElement("li");
         itemList.setAttribute( "id", "itemlist");
         let itemListText = document.createTextNode(`The numbers added appear here.`);
@@ -40,8 +64,9 @@ function toClean() {
         position.appendChild(itemList);
         results?.remove();
     }
+    txtNumber.focus();
 }
-// Create Elements Array
+/* // Create Elements Array
 let addNumbers = document.querySelector("#toaddnumber");
 let arrayNumbers = [];
 function createArray() {
@@ -52,7 +77,7 @@ function createArray() {
 function deleteArray() {
     arrayNumbers.pop();
     console.log(arrayNumbers);
-}
+} */
 // Button Finalize
 let buttonFinalize = document.querySelector("#buttonfinalize");
 buttonFinalize.onclick = function toFinalize() {
